e8c6c4f6040b43ca02f7007eb1c7c258480e8148
git-dis-git-dat <too5juan@protonmail.com>
2024-07-30 14:32:27 -0400

    add n squares

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const container = document.querySelector('#container');
 
-// test 
-// container.textContent = 'rudy'
-
 function addTile(){
-    //create a new div
-    //add class for styling
-    //add it to container
-
     const tile = document.createElement('div');
     tile.className = 'tile'
     tile.textContent = 'rudy'
     container.appendChild(tile);
-
-    
 }
 
-addTile();
+// addTile();
+
+function addTiles(number = 2){
+    const totalTiles = number *= number;
+
+    for(i=0; i < totalTiles; i++){
+        const tile = document.createElement('div');
+        tile.className = 'tile'
+        container.appendChild(tile);
+    }  
+}
+
+addTiles();
