e9ae5807d2707ab062cd68a2f1fdb847895f42cc
git-dis-git-dat <too5juan@protonmail.com>
2024-07-30 10:16:18 -0400

    rough size and style of container div

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # etch-a-sketch
+
+1. Reference container div
+2. Add divs to this via script
+    - 
---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+const container = document.querySelector('#container');
+
+// test 
+// container.textContent = 'p3n15'
+
